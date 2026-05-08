02f681129e63760f8e2adae85a19425b7140b11a
wolffuschia <wolffuschia@gmail.com>
2026-05-08 15:16:34 +1000

    Added Dynamic Scroll Button
    Button appears once user scrolls down the page and allows the user to
    return to top of page in one click.

---
+++ src/main/resources/static/scripts/scroll-to-top.js
@@ -0,0 +1,22 @@
+//Functionality for scroll to top button when user scrolls down a page
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollBtn = document.getElementById('scroll-to-top');
+    const mainContent = document.querySelector('.main-content');
+    
+    //Change button visibility based on page scoll position
+    mainContent.addEventListener('scroll', function() {
+        if (mainContent.scrollTop > 100) { //show button after user has scrolled down 100 pixels
+            scrollBtn.style.display = 'block';
+        } else {
+            scrollBtn.style.display = 'none';
+        }
+    });
+
+    //Scroll to top of page on click
+    scrollBtn.addEventListener('click', function() {
+        mainContent.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
